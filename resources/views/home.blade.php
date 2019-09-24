9959929aeee7c40d7f816d8cdc1286026fe61243
Укиии Цар <ukiiiiiiiiiiii@gmail.com>
2019-09-24 16:44:17 +0200

    Stilizovanje pomocu Bootstrap-a

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,25 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://instagram.fbeg4-1.fna.fbcdn.net/vp/fea40a245ed1b61f46254a009c37f3cd/5E20DBBC/t51.2885-19/s150x150/11899454_403213659879109_419394403_a.jpg?_nc_ht=instagram.fbeg4-1.fna.fbcdn.net" class="rounded-circle" alt="Avatar">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>Урош Динић</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong> објава</div>
+                <div class="pr-5"><strong>23к</strong> пратиоца</div>
+                <div class="pr-5"><strong>212</strong> пратим</div>
             </div>
+            <div class="pt-3"><strong>Криминалистичко-полицијски универзитет</strong></div>
+            <div>Криминалистичко-полицијски универзитет настао је интеграцијом Више школе унутрашњих послова и Полицијске академије и њихов је правни следбеник.</div>
+            <div><a href="http://www.kpu.edu.rs/cms/">www.kpu.edu.rs</a></div>
+        </div>
+        <div class="row pt-5">
+            <div class="col-4"><img src="https://instagram.fbeg4-1.fna.fbcdn.net/vp/02c7048c842d9891ffbc020b7e52f87c/5E1AB835/t51.2885-15/e35/41652716_166953537519157_5059919492574613736_n.jpg?_nc_ht=instagram.fbeg4-1.fna.fbcdn.net&_nc_cat=101" alt="Post" class="w-100"></div>
+            <div class="col-4"><img src="https://instagram.fbeg4-1.fna.fbcdn.net/vp/24d0d7f30d95b9116d549fd2f6db0956/5E1C6166/t51.2885-15/e35/44265466_520616518350135_5601645302939537121_n.jpg?_nc_ht=instagram.fbeg4-1.fna.fbcdn.net&_nc_cat=103" alt="Post" class="w-100"></div>
+            <div class="col-4"><img src="https://instagram.fbeg4-1.fna.fbcdn.net/vp/98e6b70b4fbe2be8141ba8533205267b/5E312735/t51.2885-15/e35/42932596_2325014707722244_1016000751304524010_n.jpg?_nc_ht=instagram.fbeg4-1.fna.fbcdn.net&_nc_cat=104" alt="Post" class="w-100"></div>
         </div>
     </div>
 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Криминалистичко-полицијски универзитет | Школска полицијска испостава</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -23,8 +23,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                    <div><img src="/svg/kpuLogo.svg" class="pr-3" style="border-right: 1px groove rgba(0, 0, 0, 0.9)"></div>
+                    <div class="pl-3 pt-1" style="line-height: 1;">КРИМИНАЛИСТИЧКО<br>ПОЛИЦИЈСКИ<br>УНИВЕРЗИТЕТ</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
