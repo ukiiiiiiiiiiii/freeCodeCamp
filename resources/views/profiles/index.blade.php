5dfd1012f3b348a6567ce5f412875ad6f58021ae
Укиии Цар <ukiiiiiiiiiiii@gmail.com>
2019-09-28 18:08:48 +0200

    Upload profilne slike i automatsko kreiranje profila prilikom registracije

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row">
         <div class="col-lg-3 p-lg-5">
-            <img src="https://instagram.fbeg4-1.fna.fbcdn.net/vp/fea40a245ed1b61f46254a009c37f3cd/5E20DBBC/t51.2885-19/s150x150/11899454_403213659879109_419394403_a.jpg?_nc_ht=instagram.fbeg4-1.fna.fbcdn.net" class="rounded-circle" alt="Avatar">
+            <img src="/storage/{{ $user->profile->image }}" class="rounded-circle w-100" alt="Avatar" style="max-width: 250px">
         </div>
 
         <div class="col-lg-9 pt-lg-5">
