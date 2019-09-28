d45ee2c2c3a65a0df16c9fcf3322fc68c84776c2
Укиии Цар <ukiiiiiiiiiiii@gmail.com>
2019-09-28 16:15:56 +0200

    Link ka objavama

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,6 +2,17 @@
 
 @section('content')
     <div class="container">
-        Show
+        <div class="row">
+            <div class="col-8">
+                <img src="/storage/{{ $post->image }}" alt="објава" class="w-100">
+            </div>
+            <div class="col-4">
+                <div>
+                    <h3>{{ $post->user->username }}</h3>
+
+                    <p>{{ $post->caption }}</p>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
