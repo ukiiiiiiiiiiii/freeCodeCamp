3bc35a2d8de2b965e74d9e0070a5bd664d8a2a33
Укиии Цар <ukiiiiiiiiiiii@gmail.com>
2019-09-28 17:26:49 +0200

    Ogranicavanje prostupa

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -6,17 +6,25 @@
         <div class="col-lg-3 p-lg-5">
             <img src="https://instagram.fbeg4-1.fna.fbcdn.net/vp/fea40a245ed1b61f46254a009c37f3cd/5E20DBBC/t51.2885-19/s150x150/11899454_403213659879109_419394403_a.jpg?_nc_ht=instagram.fbeg4-1.fna.fbcdn.net" class="rounded-circle" alt="Avatar">
         </div>
+
         <div class="col-lg-9 pt-lg-5">
             <div class="d-flex justify-content-between align-items-baseline">
                 <h1>{{ $user->username }}</h1>
-                <a href="/p/create">Додај нову објаву</a>
+                @can('update', $user->profile)
+                    <a href="/p/create">Додај нову објаву</a>
+                @endcan
             </div>
-            <a href="/profile/{{ $user->id }}/edit">Измени профил</a>
+
+            @can('update', $user->profile)
+                <a href="/profile/{{ $user->id }}/edit">Измени профил</a>
+            @endcan
+
             <div class="d-flex">
                 <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> објавe</div>
                 <div class="pr-5"><strong>23к</strong> пратиоца</div>
                 <div class="pr-5"><strong>212</strong> пратим</div>
             </div>
+
             <div class="pt-3"><strong>{{ $user->profile->title }}</strong></div>
             <div>{{ $user->profile->description }}</div>
             <div><a href="{{ $user->profile->url }}">{{ $user->profile->url }}</a></div>
